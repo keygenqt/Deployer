45402332e3ee6fa5dbb7ce4083e69487d71c4246
Vitaliy Zarubin <keygenqt@pinxterapp.com>
2020-05-07 02:43:16 +0300

    update server

--- server/internal_commits.sh
+++ server/internal_commits.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 lastDate=$(eval git log -1 --grep='internal' -i -E --format=%cd HEAD)
+
+if [ -z "$lastDate" ]; then
+  lastDate=$(git rev-list --max-parents=0 --format=%cd HEAD | sed -n 2p)
+fi
+
 logs=$(git log -30 --after="$lastDate" --grep='\[Feature\]|\[Bug\]|\[Change\]|\[Test\]' -i -E --format='* '%B\(%an\) HEAD | sed -e ":a;$!{N;s/\n/ /;ba;}" | sed -e "s/*/\n/g")
 
 readarray -t array <<<"$logs"
